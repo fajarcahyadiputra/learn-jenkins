9984f6610745cf6241aa1033e75a3dbad593ec0a
Fajar Cahyadi Putra <fajarcahyadiputra@gmail.com>
2025-06-22 18:08:56 +0700

    update greeting message in main.go to include additional text "eek"

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/hello", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "Hello World. Asuuuuuuu",
+			"message": "Hello World. Asuuuuuuu eek",
 		})
 	})
 	r.GET("/hello/:name", func(c *gin.Context) {
